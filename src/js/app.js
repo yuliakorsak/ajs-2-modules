e2873d0cbfad94648c35a3bb9631a1d8ebfc2cd3
Yulia Ermolaeva <168757304+yuliakorsak@users.noreply.github.com>
2024-10-25 17:25:15 +0300

    Add domain.js, game.js; Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,4 @@
-// TODO: write your code here
-import sum from './basic';
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js'
 
-console.log('worked');
-
-console.log(sum([1, 2]));
+const game = new Game();
+game.start();
